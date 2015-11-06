1a409d4a2c38b64116f63b8d9164e81f989ae193
CodyGuo <guoxiao219@126.com>
2015-11-06 13:02:57 +0800

    增加 frame注释

--- xc/frame.go
+++ xc/frame.go
@@ -28,7 +28,16 @@ func init() {
     XFrameWnd_AddPane = XCDLL.MustFindProc("XFrameWnd_AddPane")
 }
 
-// 创建框架窗口.
+// *******************************************
+// @Author: cody.guo
+// @Date: 2015-11-6 12:59:41
+// @Function: XFrameWndCreate
+// @Description: 创建框架窗口.
+// @Calls: XPane_Create
+// @Input: x 窗口左上角x坐标. y 窗口左上角y坐标. cx 窗口宽度. cy 窗口高度. pTitle 窗口标题. hWndParent 父窗口.
+//         XCStyle GUI库窗口样式,样式请参见api 常量定义 xc_window_style_.
+// @Return: GUI库窗口资源句柄.
+// *******************************************
 func XFrameWndCreate(x, y, cx, cy int, pTitle string, hWndParent HWND, XCStyle uint32) HWINDOW {
     // API: XFrameWnd_Create
     ret, _, _ := XFrameWnd_Create.Call(
@@ -43,9 +52,16 @@ func XFrameWndCreate(x, y, cx, cy int, pTitle string, hWndParent HWND, XCStyle u
     return HWINDOW(ret)
 }
 
-// 创建窗格元素.
+// *******************************************
+// @Author: cody.guo
+// @Date: 2015-11-6 12:53:14
+// @Function: XPaneCreate
+// @Description: 创建窗格元素.
+// @Calls: XPane_Create
+// @Input: pName [窗格标题]. nWidth [宽度]. nHeight [高度]. hFrameWnd [框架窗口].
+// @Return: 元素句柄.
+// *******************************************
 func XPaneCreate(pName string, nWidth, nHeight int, hFrameWnd HWINDOW) HELE {
-    // API: XPane_Create
     ret, _, _ := XPane_Create.Call(
         StringToUintPtr(pName),
         uintptr(nWidth),
@@ -61,11 +77,10 @@ func XPaneCreate(pName string, nWidth, nHeight int, hFrameWnd HWINDOW) HELE {
 // @Function: XFrameWndAddPane
 // @Description: 添加窗格到框架窗口.
 // @Calls: XFrameWnd_AddPane
-// @Input: align -> align_type_
+// @Input: hWindow [窗口句柄]. hPaneDest [目标窗格]. hPaneNew [当前窗格].  align [对齐方式].
 // @Return: 成功返回TRUE否则返回FALSE.
 // *******************************************
 func XFrameWndAddPane(hWindow HWINDOW, hPaneDest HELE, hPaneNew HELE, align int32) bool {
-    // API: XFrameWnd_AddPane
     ret, _, _ := XFrameWnd_AddPane.Call(
         uintptr(hWindow),
         uintptr(hPaneDest),

9fd3a95f5be15c1353cec60763d6fa7c0e6f3964
xicheng <dict.new@gmail.com>
2015-11-24 22:22:13 +0800

    ToolBar

---
+++ xc/toolBar.go
@@ -0,0 +1,99 @@
+package xc
+
+import (
+	"syscall"
+)
+
+var (
+	xToolBar_Create           *syscall.Proc
+	xToolBar_InsertEle        *syscall.Proc
+	xToolBar_InsertSeparator  *syscall.Proc
+	xToolBar_EnableButtonMenu *syscall.Proc
+	xToolBar_GetHEle          *syscall.Proc
+	xToolBar_GetButtonLeft    *syscall.Proc
+	xToolBar_GetButtonRight   *syscall.Proc
+	xToolBar_GetButtonMenu    *syscall.Proc
+	xToolBar_SetSpace         *syscall.Proc
+	xToolBar_DeleteEle        *syscall.Proc
+	xToolBar_DeleteAllEle     *syscall.Proc
+)
+
+func init() {
+	xToolBar_Create = XCDLL.MustFindProc("XToolBar_Create")
+	xToolBar_InsertEle = XCDLL.MustFindProc("XToolBar_InsertEle")
+	xToolBar_InsertSeparator = XCDLL.MustFindProc("XToolBar_InsertSeparator")
+	xToolBar_EnableButtonMenu = XCDLL.MustFindProc("XToolBar_EnableButtonMenu")
+	xToolBar_GetHEle = XCDLL.MustFindProc("XToolBar_GetHEle")
+	xToolBar_GetButtonLeft = XCDLL.MustFindProc("XToolBar_GetButtonLeft")
+	xToolBar_GetButtonRight = XCDLL.MustFindProc("XToolBar_GetButtonRight")
+	xToolBar_GetButtonMenu = XCDLL.MustFindProc("XToolBar_GetButtonMenu")
+	xToolBar_SetSpace = XCDLL.MustFindProc("XToolBar_SetSpace")
+	xToolBar_DeleteEle = XCDLL.MustFindProc("XToolBar_DeleteEle")
+	xToolBar_DeleteAllEle = XCDLL.MustFindProc("XToolBar_DeleteAllEle")
+}
+
+func XToolBarCreate(x int, y int, cx int, cy int, hParent HXCGUI) HELE {
+	ret, _, _ := xTextLink_Create.Call(
+		uintptr(x),
+		uintptr(y),
+		uintptr(cx),
+		uintptr(cy),
+		uintptr(hParent))
+	return HELE(ret)
+}
+
+func XToolBarInsertEle(hEle HELE, hNewEle HELE, index int) int {
+	ret, _, _ := xToolBar_InsertEle.Call(
+		uintptr(hEle),
+		uintptr(hNewEle),
+		uintptr(index))
+	return int(ret)
+}
+func XToolBarInsertSeparator(hEle HELE, index int) int {
+	ret, _, _ := xToolBar_InsertSeparator.Call(
+		uintptr(hEle),
+		uintptr(index))
+	return int(ret)
+}
+
+func XToolBarEnableButtonMenu(hEle HELE, bEnable bool) {
+	xToolBar_EnableButtonMenu.Call(
+		uintptr(hEle),
+		uintptr(BoolToBOOL(bEnable)))
+
+}
+
+func XToolBarGetHEle(hEle HELE, index int) HELE {
+	ret, _, _ := xToolBar_GetHEle.Call(
+		uintptr(hEle),
+		uintptr(index))
+	return HELE(ret)
+}
+
+func XToolBarGetButtonLeft(hEle HELE) HELE {
+	ret, _, _ := xToolBar_GetButtonLeft.Call(
+		uintptr(hEle))
+	return HELE(ret)
+}
+
+func XToolBarGetButtonRight(hEle HELE) HELE {
+	ret, _, _ := xToolBar_GetButtonRight.Call(
+		uintptr(hEle))
+	return HELE(ret)
+}
+
+func XToolBarGetButtonMenu(hEle HELE) HELE {
+	ret, _, _ := xToolBar_GetButtonMenu.Call(
+		uintptr(hEle))
+	return HELE(ret)
+}
+
+func XToolBarSetSpace(hEle HELE, nSize int) {
+	xToolBar_SetSpace.Call(uintptr(hEle), uintptr(nSize))
+}
+func XToolBarDeleteEle(hEle HELE, index int) {
+	xToolBar_DeleteEle.Call(uintptr(hEle), uintptr(index))
+}
+func XToolBarDeleteAllEle(hEle HELE) {
+	xToolBar_DeleteAllEle.Call(uintptr(hEle))
+}

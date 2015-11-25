9fe3b23775e323ffbb01503e271b641f6baa9936
xicheng <dict.new@gmail.com>
2015-11-25 16:30:06 +0800

    sliderBar

---
+++ xc/sliderBar.go
@@ -0,0 +1,104 @@
+package xc
+
+import (
+	"syscall"
+)
+
+var (
+	xSliderBar_Create          *syscall.Proc
+	xSliderBar_SetRange        *syscall.Proc
+	xSliderBar_GetRange        *syscall.Proc
+	xSliderBar_SetImageLoad    *syscall.Proc
+	xSliderBar_SetButtonWidth  *syscall.Proc
+	xSliderBar_SetButtonHeight *syscall.Proc
+	xSliderBar_SetSpaceTwo     *syscall.Proc
+	xSliderBar_SetPos          *syscall.Proc
+	xSliderBar_GetPos          *syscall.Proc
+	xSliderBar_GetButton       *syscall.Proc
+	xSliderBar_SetHorizon      *syscall.Proc
+)
+
+func init() {
+	xSliderBar_Create = XCDLL.MustFindProc("XSliderBar_Create")
+	xSliderBar_SetRange = XCDLL.MustFindProc("XSliderBar_SetRange")
+	xSliderBar_GetRange = XCDLL.MustFindProc("XSliderBar_GetRange")
+	xSliderBar_SetImageLoad = XCDLL.MustFindProc("XSliderBar_SetImageLoad")
+	xSliderBar_SetButtonWidth = XCDLL.MustFindProc("XSliderBar_SetButtonWidth")
+	xSliderBar_SetButtonHeight = XCDLL.MustFindProc("XSliderBar_SetButtonHeight")
+	xSliderBar_SetSpaceTwo = XCDLL.MustFindProc("XSliderBar_SetSpaceTwo")
+	xSliderBar_SetPos = XCDLL.MustFindProc("XSliderBar_SetPos")
+	xSliderBar_GetPos = XCDLL.MustFindProc("XSliderBar_GetPos")
+	xSliderBar_GetButton = XCDLL.MustFindProc("XSliderBar_GetButton")
+	xSliderBar_SetHorizon = XCDLL.MustFindProc("XSliderBar_SetHorizon")
+}
+
+func XSliderBarCreate(x int, y int, cx int, cy int, hParent HXCGUI) HELE {
+	ret, _, _ := xSliderBar_Create.Call(
+		uintptr(x),
+		uintptr(y),
+		uintptr(cx),
+		uintptr(cy),
+		uintptr(hParent))
+
+	return HELE(ret)
+}
+
+func XSliderBarSetRange(hEle HELE, irange int) {
+	xSliderBar_SetRange.Call(
+		uintptr(hEle),
+		uintptr(irange))
+}
+
+func XSliderBarGetRange(hEle HELE) int {
+	ret, _, _ := xSliderBar_GetRange.Call(
+		uintptr(hEle))
+	return int(ret)
+}
+
+func XSliderBarSetImageLoad(hEle HELE, hImage HIMAGE) {
+	xSliderBar_SetImageLoad.Call(
+		uintptr(hEle),
+		uintptr(hImage))
+}
+
+func XSliderBarSetButtonWidth(hEle HELE, width int) {
+	xSliderBar_SetButtonWidth.Call(
+		uintptr(hEle),
+		uintptr(width))
+}
+
+func XSliderBarSetButtonHeight(hEle HELE, height int) {
+	xSliderBar_SetButtonHeight.Call(
+		uintptr(hEle),
+		uintptr(height))
+}
+
+func XSliderBarSetSpaceTwo(hEle HELE, leftSize int, rightSize int) {
+	xSliderBar_SetSpaceTwo.Call(
+		uintptr(hEle),
+		uintptr(leftSize),
+		uintptr(rightSize))
+
+}
+
+func XSliderBarSetPos(hEle HELE, pos int) {
+	xSliderBar_SetPos.Call(
+		uintptr(hEle),
+		uintptr(pos))
+}
+
+func XSliderBarGetPos(hEle HELE) int {
+	ret, _, _ := xSliderBar_GetPos.Call(uintptr(hEle))
+	return int(ret)
+}
+
+func XSliderBarGetButton(hEle HELE) HELE {
+	ret, _, _ := xSliderBar_GetButton.Call(uintptr(hEle))
+	return HELE(ret)
+}
+
+func XSliderBarSetHorizon(hEle HELE, bHorizon bool) {
+	xSliderBar_SetHorizon.Call(
+		uintptr(hEle),
+		uintptr(BoolToBOOL(bHorizon)))
+}

264b10d192a6746c31c168c954b20dc9a8eb1e40
CodyGuo <guoxiao219@126.com>
2015-11-29 17:09:33 +0800

    完成comboBox注释

--- xc/comboBox.go
+++ xc/comboBox.go
@@ -49,6 +49,18 @@ func init() {
 	xComboBox_ClearBkInfo = XCDLL.MustFindProc("XComboBox_ClearBkInfo")
 }
 
+/*
+创建下拉组合框元素.
+
+参数:
+	x 元素x坐标.
+	y 元素y坐标.
+	cx 宽度.
+	cy 高度.
+	hParent 父是窗口资源句柄或UI元素资源句柄.如果是窗口资源句柄将被添加到窗口, 如果是元素资源句柄将被添加到元素.
+返回:
+	元素句柄.
+*/
 func XComboBoxCreate(x int, y int, cx int, cy int, hParent HXCGUI) HELE {
 	ret, _, _ := xComboBox_Create.Call(
 		uintptr(x),
@@ -59,6 +71,15 @@ func XComboBoxCreate(x int, y int, cx int, cy int, hParent HXCGUI) HELE {
 	return HELE(ret)
 }
 
+/*
+设置选择项.
+
+参数:
+	hEle 元素句柄.
+	iIndex 想索引.
+返回:
+	成功返回否则返回FALSE.
+*/
 func XComboBoxSetSelItem(hEle HELE, iIndex int) bool {
 	ret, _, _ := xComboBox_SetSelItem.Call(
 		uintptr(hEle),
@@ -67,59 +88,133 @@ func XComboBoxSetSelItem(hEle HELE, iIndex int) bool {
 
 }
 
+/*
+绑定数据适配器.
+
+参数:
+	hEle 元素句柄.
+	hAdapter 适配器句柄.
+*/
 func XComboBoxBindApapter(hEle HELE, hAdapter HXCGUI) {
 	xComboBox_BindApapter.Call(
 		uintptr(hEle),
 		uintptr(hAdapter))
 }
 
+/*
+获取下拉按钮坐标.
+
+参数:
+	hEle 元素句柄.
+	pRect 坐标.
+*/
 func XComboBoxGetButtonRect(hEle HELE, pRect *RECT) {
 	xComboBox_GetButtonRect.Call(
 		uintptr(hEle),
 		uintptr(unsafe.Pointer(pRect)))
 }
 
+/*
+设置下拉按钮大小.
+
+参数:
+	hEle 元素句柄.
+	size 大小.
+*/
 func XComboBoxSetButtonSize(hEle HELE, size int) {
 	xComboBox_SetButtonSize.Call(
 		uintptr(hEle),
 		uintptr(size))
 }
 
+/*
+设置下拉列表高度.
+
+参数:
+	hEle 元素句柄.
+	height 高度.
+*/
 func XComboBoxSetDropHeight(hEle HELE, height int) {
 	xComboBox_SetDropHeight.Call(
 		uintptr(hEle),
 		uintptr(height))
 }
 
+/*
+获取下拉列表高度.
+
+参数:
+	hEle 元素句柄.
+返回:
+	下拉列表高度.
+*/
 func XComboBoxGetDropHeight(hEle HELE) int {
 	ret, _, _ := xComboBox_GetDropHeight.Call(uintptr(hEle))
 	return int(ret)
 }
 
+/*
+设置下拉列表项模板文件.
+
+参数:
+	hEle 元素句柄.
+	pXmlFile 项模板文件.
+*/
 func XComboBoxSetItemTemplateXML(hEle HELE, pXmlFile string) {
 	xComboBox_SetItemTemplateXML.Call(
 		uintptr(hEle),
 		StringToUintPtr(pXmlFile))
 }
 
+/*
+设置下拉列表项模板.
+
+参数:
+	hEle 元素句柄.
+	pStringXML 字符串指针.
+*/
 func XComboBoxSetItemTemplateXMLFromString(hEle HELE, pStringXML string) {
 	xComboBox_SetItemTemplateXMLFromString.Call(
 		uintptr(hEle),
 		StringToUintPtr(pStringXML))
 }
 
+/*
+是否绘制下拉按钮.
+
+参数:
+	hEle 元素句柄.
+	bEnable 是否绘制.
+*/
 func XComboBoxEnableDrawButton(hEle HELE, bEnable bool) {
 	xComboBox_EnableDrawButton.Call(
 		uintptr(hEle),
 		uintptr(BoolToBOOL(bEnable)))
 }
 
+/*
+启用可编辑显示的文本内容.
+
+参数:
+	hEle 元素句柄.
+	bEdit TRUE可编辑,否则相反.
+*/
 func XComboBoxEnableEdit(hEle HELE, bEdit bool) {
 	xComboBox_EnableEdit.Call(
 		uintptr(hEle),
 		uintptr(BoolToBOOL(bEdit)))
 }
 
+/*
+添加背景内容边框.
+
+参数:
+	hEle 元素句柄.
+	nState 按钮状态.
+	color RGB颜色.
+	alpha 透明度.
+	width 线宽.
+*/
 func XComboBoxAddBkBorder(hEle HELE, nState ComboBox_state_, color COLORREF, alpha byte, width int) {
 	xComboBox_AddBkBorder.Call(
 		uintptr(hEle),
@@ -129,6 +224,14 @@ func XComboBoxAddBkBorder(hEle HELE, nState ComboBox_state_, color COLORREF, alp
 		uintptr(width))
 }
 
+/*
+添加背景内容填充.
+参数:
+	hEle 元素句柄.
+	nState 按钮状态.
+	color RGB颜色.
+	alpha 透明度.
+*/
 func XComboBoxAddBkFill(hEle HELE, nState ComboBox_state_, color COLORREF, alpha byte) {
 	xComboBox_AddBkFill.Call(
 		uintptr(hEle),
@@ -137,6 +240,14 @@ func XComboBoxAddBkFill(hEle HELE, nState ComboBox_state_, color COLORREF, alpha
 		uintptr(alpha))
 }
 
+/*
+添加背景内容图片.
+
+参数:
+	hEle 元素句柄.
+	nState 按钮状态.
+	hImage 图片句柄.
+*/
 func XComboBoxAddBkImage(hEle HELE, nState ComboBox_state_, hImage HIMAGE) {
 	xComboBox_AddBkImage.Call(
 		uintptr(hEle),
@@ -144,6 +255,15 @@ func XComboBoxAddBkImage(hEle HELE, nState ComboBox_state_, hImage HIMAGE) {
 		uintptr(hImage))
 }
 
+/*
+获取背景内容数量.
+
+参数:
+	hEle 元素句柄.
+	nState 按钮状态.
+返回:
+	成功返回背景内容数量,否则返回XC_ID_ERROR.
+*/
 func XComboboxGetBkInfoCount(hEle HELE, nState ComboBox_state_) int {
 	ret, _, _ := xCombobox_GetBkInfoCount.Call(
 		uintptr(hEle),
@@ -151,12 +271,28 @@ func XComboboxGetBkInfoCount(hEle HELE, nState ComboBox_state_) int {
 	return int(ret)
 }
 
+/*
+清空背景内容; 如果背景没有内容,将使用系统默认内容,以便保证背景正确.
+
+参数:
+	hEle 元素句柄.
+	nState 按钮状态.
+*/
 func XComboBoxClearBkInfo(hEle HELE, nState ComboBox_state_) {
 	xComboBox_ClearBkInfo.Call(
 		uintptr(hEle),
 		uintptr(nState))
 }
 
+/*
+获取背景内容管理器.
+
+参数:
+	hEle 元素句柄.
+	nState 状态.
+返回:
+	背景内容管理器.
+*/
 func XComboBoxGetBkInfoManager(hEle HELE, nState ComboBox_state_) HBKINFOM {
 	ret, _, _ := xComboBox_GetBkInfoManager.Call(
 		uintptr(hEle),
@@ -164,12 +300,28 @@ func XComboBoxGetBkInfoManager(hEle HELE, nState ComboBox_state_) HBKINFOM {
 	return HBKINFOM(ret)
 }
 
+/*
+获取组合框下拉列表中选择项索引.
+
+参数:
+	hEle 元素句柄.
+返回:
+	返回项索引.
+*/
 func XComboBoxGetSelItem(hEle HELE) int {
 	ret, _, _ := xComboBox_GetSelItem.Call(
 		uintptr(hEle))
 	return int(ret)
 }
 
+/*
+获取状态.
+
+参数:
+	hEle 元素句柄.
+返回:
+	状态.
+*/
 func XComboBoxGetState(hEle HELE) ComboBox_state_ {
 	ret, _, _ := xComboBox_GetState.Call(
 		uintptr(hEle))

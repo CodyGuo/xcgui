e5d4ef941c5b22975b794a376ad26ebf3476f915
CodyGuo <guoxiao219@126.com>
2015-11-30 21:38:01 +0800

    frameWindow增加注释，修正类型

--- xc/frameWindow.go
+++ xc/frameWindow.go
@@ -2,6 +2,7 @@ package xc
 
 import (
 	"syscall"
+	"unsafe"
 )
 
 // align_type_
@@ -36,45 +37,55 @@ func init() {
 	xFrameWnd_MergePane = xcDLL.MustFindProc("XFrameWnd_MergePane")
 }
 
-// *******************************************
-// @Author: cody.guo
-// @Date: 2015-11-6 12:59:41
-// @Function: XFrameWndCreate
-// @Description: 创建框架窗口.
-// @Calls: xFrameWnd_Create
-// @Input: x 窗口左上角x坐标. y 窗口左上角y坐标. cx 窗口宽度. cy 窗口高度. pTitle 窗口标题. hWndParent 父窗口.
-//         XCStyle GUI库窗口样式,样式请参见api 常量定义 xc_window_style_.
-// @Return: GUI库窗口资源句柄.
-// *******************************************
-func XFrameWndCreate(x, y, cx, cy int, pTitle string, hWndParent HWND, XCStyle uint32) HWINDOW {
+/*
+创建框架窗口
+
+参数:
+	x 窗口左上角x坐标.
+	y 窗口左上角y坐标.
+	cx 窗口宽度.
+	cy 窗口高度.
+	pTitle 窗口标题.
+	hWndParent 父窗口.
+	XCStyle GUI库窗口样式,样式请参见宏定义 xc_window_style_.
+返回:
+	GUI库窗口资源句柄.
+*/
+func XFrameWndCreate(x, y, cx, cy int, pTitle *uint16, hWndParent HWND, XCStyle int) HWINDOW {
 	ret, _, _ := xFrameWnd_Create.Call(
 		uintptr(x),
 		uintptr(y),
 		uintptr(cx),
 		uintptr(cy),
-		StringToUintPtr(pTitle),
+		uintptr(unsafe.Pointer(pTitle)),
 		uintptr(hWndParent),
 		uintptr(XCStyle))
 
 	return HWINDOW(ret)
 }
 
-// *******************************************
-// @Author: cody.guo
-// @Date: 2015-11-6 20:20:46
-// @Function: XFrameWndCreateEx
-// @Description: 创建框架窗口,增强功能.
-// @Calls: xFrameWnd_CreateEx
-// @Input: dwExStyle 窗口扩展样式. lpClassName 窗口类名. lpWindowName 窗口名. dwStyle 窗口样式
-//         x 窗口左上角x坐标. y 窗口左上角y坐标. cx 窗口宽度. cy 窗口高度. hWndParent 父窗口.
-//         XCStyle GUI库窗口样式,样式请参见api 常量定义 xc_window_style_.
-// @Return: GUI库窗口资源句柄.
-// *******************************************
-func XFrameWndCreateEx(dwExStyle uint32, lpClassName, lpWindowName string, dwStyle uint32, x, y, cx, cy int, hWndParent HWND, XCStyle uint32) HWINDOW {
+/*
+创建框架窗口,增强功能.
+
+参数:
+	dwExStyle 窗口扩展样式.
+	lpClassName 窗口类名.
+	lpWindowName 窗口名.
+	dwStyle 窗口样式
+	x 窗口左上角x坐标.
+	y 窗口左上角y坐标.
+	cx 窗口宽度.
+	cy 窗口高度.
+	hWndParent 父窗口.
+	XCStyle GUI库窗口样式,样式请参见宏定义 xc_window_style_.
+返回:
+	GUI库窗口资源句柄.
+*/
+func XFrameWndCreateEx(dwExStyle int, lpClassName, lpWindowName *uint16, dwStyle int, x, y, cx, cy int, hWndParent HWND, XCStyle uint32) HWINDOW {
 	ret, _, _ := xFrameWnd_CreateEx.Call(
 		uintptr(dwExStyle),
-		StringToUintPtr(lpClassName),
-		StringToUintPtr(lpWindowName),
+		uintptr(unsafe.Pointer(lpClassName)),
+		uintptr(unsafe.Pointer(lpWindowName)),
 		uintptr(dwStyle),
 		uintptr(x),
 		uintptr(y),
@@ -86,61 +97,58 @@ func XFrameWndCreateEx(dwExStyle uint32, lpClassName, lpWindowName string, dwSty
 	return HWINDOW(ret)
 }
 
-// *******************************************
-// @Author: cody.guo
-// @Date: 2015-11-6 20:28:09
-// @Function: XFrameWndGetLayoutAreaRect
-// @Description: 用来布局窗格的区域坐标,不包含码头.
-// @Calls: xFrameWnd_GetLayoutAreaRect
-// @Input: hWindow 窗口句柄. pRect 返回坐标.
-// @Return:
-// *******************************************
-func XFrameWndGetLayoutAreaRect(hWindow HWINDOW, pRect uint32) {
+/*
+用来布局窗格的区域坐标,不包含码头.
+
+参数:
+	hWindow 窗口句柄.
+	pRect 返回坐标.
+*/
+func XFrameWndGetLayoutAreaRect(hWindow HWINDOW, pRect *RECT) {
 	xFrameWnd_GetLayoutAreaRect.Call(
 		uintptr(hWindow),
-		uintptr(pRect))
+		uintptr(unsafe.Pointer(pRect)))
 }
 
-// *******************************************
-// @Author: cody.guo
-// @Date: 2015-11-6 20:35:53
-// @Function: XFrameWndSetView
-// @Description: 设置主视图元素.
-// @Calls: xFrameWnd_SetView
-// @Input: hWindow 窗口句柄. hEle 元素句柄.
-// @Return:
-// *******************************************
+/*
+设置主视图元素.
+
+参数:
+	hWindow 窗口句柄.
+	hEle 元素句柄.
+*/
 func XFrameWndSetView(hWindow HWINDOW, hEle HELE) {
 	xFrameWnd_SetView.Call(
 		uintptr(hWindow),
 		uintptr(hEle))
 }
 
-// *******************************************
-// @Author: cody.guo
-// @Date: 2015-11-6 20:38:37
-// @Function: XFrameWndSetPaneSplitBarColor
-// @Description: 设置窗格分隔条颜色.
-// @Calls: xFrameWnd_SetPaneSplitBarColor
-// @Input: hWindow 窗口句柄. color RGB颜色值. alpha 透明度.
-// @Return:
-// *******************************************
-func XFrameWndSetPaneSplitBarColor(hWindow HWINDOW, color COLORREF, alpha int) {
+/*
+设置窗格分隔条颜色.
+
+参数:
+	hWindow 窗口句柄.
+	color RGB颜色值.
+	alpha 透明度.
+*/
+func XFrameWndSetPaneSplitBarColor(hWindow HWINDOW, color COLORREF, alpha byte) {
 	xFrameWnd_SetPaneSplitBarColor.Call(
 		uintptr(hWindow),
 		uintptr(color),
 		uintptr(alpha))
 }
 
-// *******************************************
-// @Author: cody.guo
-// @Date: 2015-11-4 23:46:41
-// @Function: XFrameWndAddPane
-// @Description: 添加窗格到框架窗口.
-// @Calls: xFrameWnd_AddPane
-// @Input: hWindow 窗口句柄. hPaneDest 目标窗格. hPaneNew 当前窗格.  align 对齐方式.
-// @Return: 成功返回TRUE否则返回FALSE.
-// *******************************************
+/*
+添加窗格到框架窗口.
+
+参数:
+	hWindow 窗口句柄.
+	hPaneDest 目标窗格.
+	hPaneNew 当前窗格.
+	align 对齐方式.
+返回:
+	成功返回TRUE否则返回FALSE.
+*/
 func XFrameWndAddPane(hWindow HWINDOW, hPaneDest HELE, hPaneNew HELE, align int32) bool {
 	ret, _, _ := xFrameWnd_AddPane.Call(
 		uintptr(hWindow),
@@ -148,31 +156,24 @@ func XFrameWndAddPane(hWindow HWINDOW, hPaneDest HELE, hPaneNew HELE, align int3
 		uintptr(hPaneNew),
 		uintptr(align))
 
-	if ret != TRUE {
-		return false
-	}
-
-	return true
+	return ret == TRUE
 }
 
-// *******************************************
-// @Author: cody.guo
-// @Date: 2015-11-6 19:24:15
-// @Function: XFrameWndMergePane
-// @Description: 合并窗格.
-// @Calls:  xFrameWnd_MergePane
-// @Input: hWindow 窗口句柄. hPaneDest 目标窗格. hPaneNew 当前窗格.
-// @Return: 成功返回TRUE否则返回FALSE.
-// *******************************************
+/*
+合并窗格.
+
+参数:
+	hWindow 窗口句柄.
+	hPaneDest 目标窗格.
+	hPaneNew 当前窗格.
+返回:
+	成功返回TRUE否则返回FALSE.
+*/
 func XFrameWndMergePane(hWindow HWINDOW, hPaneDest, hPaneNew HELE) bool {
 	ret, _, _ := xFrameWnd_MergePane.Call(
 		uintptr(hWindow),
 		uintptr(hPaneDest),
 		uintptr(hPaneNew))
 
-	if ret != TRUE {
-		return false
-	}
-
-	return true
+	return ret == TRUE
 }

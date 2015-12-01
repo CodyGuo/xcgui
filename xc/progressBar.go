f532ab1439386e074e3f0c64974d810af40f009a
CodyGuo <guoxiao219@126.com>
2015-12-01 23:14:53 +0800

    完成progressBar注释，修正类型

--- xc/progressBar.go
+++ xc/progressBar.go
@@ -26,6 +26,18 @@ func init() {
 	xProgBar_SetHorizon = xcDLL.MustFindProc("XProgBar_SetHorizon")
 }
 
+/*
+创建进度条元素.
+
+参数:
+	x 元素x坐标.
+	y 元素y坐标.
+	cx 宽度.
+	cy 高度.
+	hParent 父是窗口资源句柄或UI元素资源句柄.如果是窗口资源句柄将被添加到窗口, 如果是元素资源句柄将被添加到元素.
+返回:
+	元素句柄.
+*/
 func XProgBarCreate(x int, y int, cx int, cy int, hParent HXCGUI) HELE {
 	ret, _, _ := xProgBar_Create.Call(
 		uintptr(x),
@@ -33,35 +45,101 @@ func XProgBarCreate(x int, y int, cx int, cy int, hParent HXCGUI) HELE {
 		uintptr(cx),
 		uintptr(cy),
 		uintptr(hParent))
+
 	return HELE(ret)
 }
 
-func XProgBarSetRange(hEle HELE, irange int) {
-	xProgBar_SetRange.Call(uintptr(hEle), uintptr(irange))
+/*
+设置范围.
+
+参数:
+	hEle 元句柄.
+	range 范围.
+*/
+func XProgBarSetRange(hEle HELE, barRange int) {
+	xProgBar_SetRange.Call(
+		uintptr(hEle),
+		uintptr(barRange))
 }
 
+/*
+获取范围.
+
+参数:
+	hEle 元句柄.
+返回:
+	返回范围.
+*/
 func XProgBarGetRange(hEle HELE) int {
 	ret, _, _ := xProgBar_GetRange.Call(uintptr(hEle))
+
 	return int(ret)
 }
 
+/*
+设置进度贴图.
+
+参数:
+	hEle 元句柄.
+	hImage 图片句柄.
+*/
 func XProgBarSetImageLoad(hEle HELE, hImage HIMAGE) {
-	xProgBar_SetImageLoad.Call(uintptr(hEle), uintptr(hImage))
+	xProgBar_SetImageLoad.Call(
+		uintptr(hEle),
+		uintptr(hImage))
 }
 
+/*
+设置两端间隔大小.
+
+参数:
+	hEle 元句柄.
+	leftSize 左边间隔大小.
+	rightSize 右边间隔大小.
+*/
 func XProgBarSetSpaceTwo(hEle HELE, leftSize int, rightSize int) {
-	xProgBar_SetSpaceTwo.Call(uintptr(hEle), uintptr(leftSize), uintptr(rightSize))
+	xProgBar_SetSpaceTwo.Call(
+		uintptr(hEle),
+		uintptr(leftSize),
+		uintptr(rightSize))
 }
 
+/*
+设置位置点.
+
+参数:
+	hEle 元句柄.
+	pos 位置点.
+*/
 func XProgBarSetPos(hEle HELE, pos int) {
-	xProgBar_SetPos.Call(uintptr(hEle), uintptr(pos))
+	xProgBar_SetPos.Call(
+		uintptr(hEle),
+		uintptr(pos))
 }
 
+/*
+获取当前位置点.
+
+参数:
+	hEle 元句柄.
+返回:
+	返回当前位置点.
+*/
 func XProgBarGetPos(hEle HELE) int {
 	ret, _, _ := xProgBar_GetPos.Call(uintptr(hEle))
+
 	return int(ret)
 }
 
+/*
+设置水平或垂直.
+
+参数:
+	hEle 元句柄.
+	bHorizon 水平或垂直.
+*/
 func XProgBarSetHorizon(hEle HELE, bHorizon bool) {
-	xProgBar_SetPos.Call(uintptr(hEle), uintptr(BoolToBOOL(bHorizon)))
+	xProgBar_SetPos.Call(
+		uintptr(hEle),
+		uintptr(BoolToBOOL(bHorizon)))
 }

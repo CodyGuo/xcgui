c7cbe9a07ab10ce5b92c93fbdcce0c05e3f606c3
CodyGuo <guoxiao219@126.com>
2015-12-01 23:02:15 +0800

    完成menuBar注释，修正类型

--- xc/menuBar.go
+++ xc/menuBar.go
@@ -2,6 +2,7 @@ package xc
 
 import (
 	"syscall"
+	"unsafe"
 )
 
 var (
@@ -20,6 +21,18 @@ func init() {
 	xMenuBar_DeleteButton = xcDLL.MustFindProc("XMenuBar_DeleteButton")
 }
 
+/*
+创建菜单条元素;如果指定了父为窗口,默认调用XWnd_AddMenuBar()函数,将菜单条添加到窗口非客户区.
+
+参数:
+	x 元素x坐标.
+	y 元素y坐标.
+	cx 宽度.
+	cy 高度.
+	hParent 父是窗口资源句柄或U I元素资源句柄.如果是窗口资源句柄将被添加到窗口, 如果是元素资源句柄将被添加到元素.
+返回:
+	元素句柄.
+*/
 func XMenuBarCreate(x int, y int, cx int, cy int, hParent HXCGUI) HELE {
 	ret, _, _ := xMenuBar_Create.Call(
 		uintptr(x),
@@ -27,22 +40,49 @@ func XMenuBarCreate(x int, y int, cx int, cy int, hParent HXCGUI) HELE {
 		uintptr(cx),
 		uintptr(cy),
 		uintptr(hParent))
+
 	return HELE(ret)
 }
 
-func XMenuBarAddButton(hEle HELE, pText string) int {
+/*
+添加弹出菜单按钮.
+
+参数:
+	hEle 元素句柄.
+	pText 文本内容.
+返回:
+	返回菜单按钮索引.
+*/
+func XMenuBarAddButton(hEle HELE, pText *uint16) int {
 	ret, _, _ := xMenuBar_AddButton.Call(
 		uintptr(hEle),
-		StringToUintPtr(pText))
+		uintptr(unsafe.Pointer(pText)))
+
 	return int(ret)
 }
 
+/*
+设置菜单按钮高度.
+
+参数:
+	hEle 元素句柄.
+	height 高度.
+*/
 func XMenuBarSetButtonHeight(hEle HELE, height int) {
 	xMenuBar_SetButtonHeight.Call(
 		uintptr(hEle),
 		uintptr(height))
 }
 
+/*
+获取菜单.
+
+参数:
+	hEle 元素句柄.
+	nIndex 菜单条上菜单按钮的索引.
+返回:
+	返回菜单句柄.
+*/
 func XMenuBarGetMenu(hEle HELE, nIndex int) HMENUX {
 	ret, _, _ := xMenuBar_GetMenu.Call(
 		uintptr(hEle),
@@ -50,9 +90,19 @@ func XMenuBarGetMenu(hEle HELE, nIndex int) HMENUX {
 	return HMENUX(ret)
 }
 
+/*
+删除菜单条上的菜单按钮,同时该按钮下的弹出菜单也被销毁.
+
+参数:
+	hEle 元素句柄.
+	nIndex 菜单条按钮索引.
+返回:
+	成功返回TRUE否则返回FALSE.
+*/
 func XMenuBarDeleteButton(hEle HELE, nIndex int) bool {
 	ret, _, _ := xMenuBar_GetMenu.Call(
 		uintptr(hEle),
 		uintptr(nIndex))
+
 	return ret == TRUE
 }

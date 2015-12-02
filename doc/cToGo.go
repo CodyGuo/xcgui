2b60e841ba49d75ac88055a3198b3258d2d9cb2f
CodyGuo <guoxiao219@126.com>
2015-12-02 20:36:49 +0800

    新增Win32 C/C++ golang 字符对照表

---
+++ doc/cToGo.go
@@ -0,0 +1,27 @@
+package doc
+
+/*
+Win32 C/C++ golang 字符对照表
+
+	WIN32类型		C/C++ 类型			GO 类型
+	HANDLE			void *				uintptr
+	BYTE			unsigned char		uint8, byte
+	SHORT			short				int16
+	WORD			unsigned short		uint16
+	INT				int					int32, int
+	UINT			unsigned int		uint32
+	LONG			long				int32
+	BOOL			int					int
+	DWORD			unsigned long		uint32
+	ULONG			unsigned long		uint32
+	CHAR			char				byte
+	WCHAR			wchar_t				uint16
+	LPSTR			char *				*byte
+	LPCSTR			const char *		*byte, syscall.StringBytePtr()
+	LPWSTR			wchar_t *			*uint16
+	LPCWSTR			const wchar_t *		*uint16, syscall.StringToUTF16Ptr()
+	FLOAT			float				float32
+	DOUBLE			double				float64
+	LONGLONG		__int64				int64
+	DWORD64			unsigned __int64	uint64
+*/

b31a884332a7e11636b87089f5b28ba5c37140ad
CodyGuo <guoxiao219@126.com>
2015-12-02 13:18:17 +0800

    完成textLink注释，修正类型

--- xc/textLink.go
+++ xc/textLink.go
@@ -2,6 +2,7 @@ package xc
 
 import (
 	"syscall"
+	"unsafe"
 )
 
 var (
@@ -22,29 +23,65 @@ func init() {
 	xTextLink_SetUnderlineColorStay = xcDLL.MustFindProc("XTextLink_SetUnderlineColorStay")
 }
 
-func XTextLinkCreate(x int, y int, cx int, cy int, pName string, hParent HXCGUI) HELE {
+/*
+创建静态文本连接元素.
+
+参数:
+	x 元素x坐标.
+	y 元素y坐标.
+	cx 宽度.
+	cy 高度.
+	pName 文本内容.
+	hParent 父是窗口资源句柄或UI元素资源句柄.如果是窗口资源句柄将被添加到窗口, 如果是元素资源句柄将被添加到元素.
+返回:
+	元素句柄.
+*/
+func XTextLinkCreate(x, y, cx, cy int, pName *uint16, hParent HXCGUI) HELE {
 	ret, _, _ := xTextLink_Create.Call(
 		uintptr(x),
 		uintptr(y),
 		uintptr(cx),
 		uintptr(cy),
-		StringToUintPtr(pName),
+		uintptr(unsafe.Pointer(pName)),
 		uintptr(hParent))
+
 	return HELE(ret)
 }
 
+/*
+启用下划线,鼠标离开状态.
+
+参数:
+	hEle 元素句柄.
+	bEnable 是否启用.
+*/
 func XTextLinkEnableUnderlineLeave(hEle HELE, bEnable bool) {
 	xTextLink_EnableUnderlineLeave.Call(
 		uintptr(hEle),
 		uintptr(BoolToBOOL(bEnable)))
 }
 
+/*
+启用下划线,鼠标停留状态.
+
+参数:
+	hEle 元素句柄.
+	bEnable 是否启用.
+*/
 func XTextLinkEnableUnderlineStay(hEle HELE, bEnable bool) {
 	xTextLink_EnableUnderlineStay.Call(
 		uintptr(hEle),
 		uintptr(BoolToBOOL(bEnable)))
 }
 
+/*
+设置文本颜色,鼠标停留状态.
+
+参数:
+	hEle 元素句柄.
+	color RGB颜色值.
+	alpha 透明度.
+*/
 func XTextLinkSetTextColorStay(hEle HELE, color COLORREF, alpha byte) {
 	xTextLink_SetTextColorStay.Call(
 		uintptr(hEle),
@@ -53,6 +90,14 @@ func XTextLinkSetTextColorStay(hEle HELE, color COLORREF, alpha byte) {
 
 }
 
+/*
+设置下划线颜色,鼠标离开状态.
+
+参数:
+	hEle 元素句柄.
+	color RGB颜色值.
+	alpha 透明度.
+*/
 func XTextLinkSetUnderlineColorLeave(hEle HELE, color COLORREF, alpha byte) {
 	xTextLink_SetUnderlineColorLeave.Call(
 		uintptr(hEle),
@@ -60,6 +105,14 @@ func XTextLinkSetUnderlineColorLeave(hEle HELE, color COLORREF, alpha byte) {
 		uintptr(alpha))
 }
 
+/*
+设置下划线颜色,鼠标停留状态.
+
+参数:
+	hEle 元素句柄.
+	color RGB颜色值.
+	alpha 透明度.
+*/
 func XTextLinkSetUnderlineColorStay(hEle HELE, color COLORREF, alpha byte) {
 	xTextLink_SetUnderlineColorStay.Call(
 		uintptr(hEle),

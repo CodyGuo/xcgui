8ca4ec361476cb90926ae5bce36aeca0f0d2804e
CodyGuo <guoxiao219@126.com>
2015-12-02 12:39:03 +0800

    完成sliderBar

--- xc/sliderBar.go
+++ xc/sliderBar.go
@@ -32,7 +32,19 @@ func init() {
 	xSliderBar_SetHorizon = xcDLL.MustFindProc("XSliderBar_SetHorizon")
 }
 
-func XSliderBarCreate(x int, y int, cx int, cy int, hParent HXCGUI) HELE {
+/*
+创建滑动条元素.
+
+参数:
+	x 元素x坐标.
+	y 元素y坐标.
+	cx 宽度.
+	cy 高度.
+	hParent 父是窗口资源句柄或UI元素资源句柄.如果是窗口资源句柄将被添加到窗口, 如果是元素资源句柄将被添加到元素.
+返回:
+	元素句柄.
+*/
+func XSliderBarCreate(x, y, cx, cy int, hParent HXCGUI) HELE {
 	ret, _, _ := xSliderBar_Create.Call(
 		uintptr(x),
 		uintptr(y),
@@ -43,37 +55,81 @@ func XSliderBarCreate(x int, y int, cx int, cy int, hParent HXCGUI) HELE {
 	return HELE(ret)
 }
 
+/*
+设置滑动范围.
+
+参数:
+	hEle 元素句柄.
+	range 范围.
+*/
 func XSliderBarSetRange(hEle HELE, irange int) {
 	xSliderBar_SetRange.Call(
 		uintptr(hEle),
 		uintptr(irange))
 }
 
+/*
+获取滚动范围.
+
+参数:
+	hEle 元素句柄.
+返回:
+	返回滚动范围.
+*/
 func XSliderBarGetRange(hEle HELE) int {
-	ret, _, _ := xSliderBar_GetRange.Call(
-		uintptr(hEle))
+	ret, _, _ := xSliderBar_GetRange.Call(uintptr(hEle))
+
 	return int(ret)
 }
 
+/*
+设置进度贴图.
+
+参数:
+	hEle 元素句柄.
+	hImage 图片句柄.
+*/
 func XSliderBarSetImageLoad(hEle HELE, hImage HIMAGE) {
 	xSliderBar_SetImageLoad.Call(
 		uintptr(hEle),
 		uintptr(hImage))
 }
 
+/*
+设置滑块按钮宽度.
+
+参数:
+	hEle 元素句柄.
+	width 宽度.
+*/
 func XSliderBarSetButtonWidth(hEle HELE, width int) {
 	xSliderBar_SetButtonWidth.Call(
 		uintptr(hEle),
 		uintptr(width))
 }
 
+/*
+设置滑块按钮高度.
+
+参数:
+	hEle 元素句柄.
+	height 高度.
+*/
 func XSliderBarSetButtonHeight(hEle HELE, height int) {
 	xSliderBar_SetButtonHeight.Call(
 		uintptr(hEle),
 		uintptr(height))
 }
 
-func XSliderBarSetSpaceTwo(hEle HELE, leftSize int, rightSize int) {
+/*
+设置两端间隔大小.
+
+参数:
+	hEle 元素句柄.
+	leftSize 左边间隔大小.
+	rightSize 右边间隔大小.
+*/
+func XSliderBarSetSpaceTwo(hEle HELE, leftSize, rightSize int) {
 	xSliderBar_SetSpaceTwo.Call(
 		uintptr(hEle),
 		uintptr(leftSize),
@@ -81,22 +137,54 @@ func XSliderBarSetSpaceTwo(hEle HELE, leftSize int, rightSize int) {
 
 }
 
+/*
+设置当前进度点.
+
+参数:
+	hEle 元素句柄.
+	pos 进度点.
+*/
 func XSliderBarSetPos(hEle HELE, pos int) {
 	xSliderBar_SetPos.Call(
 		uintptr(hEle),
 		uintptr(pos))
 }
 
+/*
+获取当前进度点.
+
+参数:
+	hEle 元素句柄.
+返回:
+	返回当前进度点.
+*/
 func XSliderBarGetPos(hEle HELE) int {
 	ret, _, _ := xSliderBar_GetPos.Call(uintptr(hEle))
+
 	return int(ret)
 }
 
+/*
+获取滑块按钮.
+
+参数:
+	hEle 元素句柄.
+返回:
+	按钮句柄.
+*/
 func XSliderBarGetButton(hEle HELE) HELE {
 	ret, _, _ := xSliderBar_GetButton.Call(uintptr(hEle))
+
 	return HELE(ret)
 }
 
+/*
+设置水平或垂直.
+
+参数:
+hEle 元素句柄.
+bHorizon 水平或垂直.
+*/
 func XSliderBarSetHorizon(hEle HELE, bHorizon bool) {
 	xSliderBar_SetHorizon.Call(
 		uintptr(hEle),

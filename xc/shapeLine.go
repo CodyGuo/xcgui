6d71c41452768c58140d21e253269e112c62b0d0
CodyGuo <guoxiao219@126.com>
2015-12-02 20:18:52 +0800

    完成shapeLine注释，修正类型

--- xc/shapeLine.go
+++ xc/shapeLine.go
@@ -16,17 +16,40 @@ func init() {
 	xShapeLine_SetColor = xcDLL.MustFindProc("XShapeLine_SetColor")
 }
 
-func XShapeLineCreate(x1, int, y1 int, x2 int, y2 int, hParent HXCGUI) HXCGUI {
+/*
+创建矩形形状对象.
+
+参数:
+	x1 坐标.
+	y1 坐标.
+	x2 坐标.
+	y2 坐标.
+	hParent 父对象句柄.
+返回:
+	返回句柄.
+*/
+func XShapeLineCreate(x1, y1, x2, y2 int, hParent HXCGUI) HXCGUI {
 	ret, _, _ := xShapeLine_Create.Call(
 		uintptr(x1),
 		uintptr(y1),
 		uintptr(x2),
 		uintptr(y2),
 		uintptr(hParent))
+
 	return HXCGUI(ret)
 }
 
-func XShapeLineSetPosition(hShape HXCGUI, x1 int, y1 int, x2 int, y2 int) {
+/*
+设置位置.
+
+参数:
+	hShape 形状对象句柄.
+	x1 坐标.
+	y1 坐标.
+	x2 坐标.
+	y2 坐标.
+*/
+func XShapeLineSetPosition(hShape HXCGUI, x1, y1, x2, y2 int) {
 	xShapeLine_SetPosition.Call(
 		uintptr(hShape),
 		uintptr(x1),
@@ -35,6 +58,14 @@ func XShapeLineSetPosition(hShape HXCGUI, x1 int, y1 int, x2 int, y2 int) {
 		uintptr(y2))
 }
 
+/*
+设置直线颜色.
+
+参数:
+	hShape 形状对象句柄.
+	color RGB颜色值.
+	alpha 透明度.
+*/
 func XShapeLineSetColor(hShape HXCGUI, color COLORREF, alpha byte) {
 	xShapeLine_SetColor.Call(
 		uintptr(hShape),

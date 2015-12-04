17e9b39b4d75158fa38545dc15401d556c51d0f4
CodyGuo <guoxiao219@126.com>
2015-12-04 13:18:22 +0800

    新增radio例子

---
+++ examples/radio/radio.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	xcgui "github.com/codyguo/xcgui/xc"
+)
+
+func main() {
+	hWindow := xcgui.XWndCreate(0, 0, 300, 200, xcgui.StringToUTF16Ptr("炫彩界面库窗口"), 0, xcgui.XC_WINDOW_STYLE_DEFAULT)
+	xcgui.XBtnSetType(xcgui.XBtnCreate(5, 3, 60, 20, xcgui.StringToUTF16Ptr("Close"), xcgui.HXCGUI(hWindow)), xcgui.BUTTON_TYPE_CLOSE)
+
+	hCheck1 := xcgui.XBtnCreate(20, 40, 100, 20, xcgui.StringToUTF16Ptr("Radio1"), xcgui.HXCGUI(hWindow))
+	hCheck2 := xcgui.XBtnCreate(20, 70, 100, 20, xcgui.StringToUTF16Ptr("Radio2"), xcgui.HXCGUI(hWindow))
+	hCheck3 := xcgui.XBtnCreate(20, 100, 100, 20, xcgui.StringToUTF16Ptr("Radio3"), xcgui.HXCGUI(hWindow))
+
+	xcgui.XBtnSetGroupID(hCheck1, 1)
+	xcgui.XBtnSetGroupID(hCheck2, 1)
+	xcgui.XBtnSetGroupID(hCheck3, 1)
+
+	xcgui.XBtnSetType(hCheck1, xcgui.BUTTON_TYPE_RADIO)
+	xcgui.XBtnSetType(hCheck2, xcgui.BUTTON_TYPE_RADIO)
+	xcgui.XBtnSetType(hCheck3, xcgui.BUTTON_TYPE_RADIO)
+
+	xcgui.XWndShowWindow(hWindow, xcgui.SW_SHOW)
+	xcgui.XRunXCGUI()
+	xcgui.XExitXCGUI()
+}

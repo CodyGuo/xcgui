2862840d964e33ec9269602b99a8c7155c153f4a
CodyGuo <guoxiao219@126.com>
2015-12-04 09:18:33 +0800

    修正hello例子

--- examples/hello/hello.go
+++ examples/hello/hello.go
@@ -11,18 +11,17 @@ import (
 
 func main() {
 	xcgui.XInitXCGUI()
-	hwnd := xcgui.XWndCreate(400, 200, 300, 200, "标题",
-		xcgui.NULL, xcgui.XC_WINDOW_STYLE_DEFAULT)
+	hwnd := xcgui.XWndCreate(400, 200, 300, 200, xcgui.StringToUTF16Ptr("标题"), xcgui.NULL, xcgui.XC_WINDOW_STYLE_DEFAULT)
 
 	parent := xcgui.HXCGUI(hwnd)
 	//button
-	btn := xcgui.XBtnCreate(10, 5, 80, 22, "关闭", parent)
+	btn := xcgui.XBtnCreate(10, 5, 80, 22, xcgui.StringToUTF16Ptr("关闭"), parent)
 	xcgui.XBtnSetType(btn, xcgui.BUTTON_TYPE_CLOSE)
 	//监听btn事件
 	xcgui.XEleRegEventC(btn, xcgui.XE_BNCLICK, syscall.NewCallback(OnBtnClick))
 	//label
-	lb := xcgui.XShapeTextCreate(50, 100, 100, 22, "hello world!", parent)
-	xcgui.XShapeTextSetText(lb, "hello 世界!")
+	lb := xcgui.XShapeTextCreate(50, 100, 100, 22, xcgui.StringToUTF16Ptr("hello world!"), parent)
+	xcgui.XShapeTextSetText(lb, xcgui.StringToUTF16Ptr("hello 世界!"))
 	xcgui.XShapeTextSetTextColor(lb, 0xff0000, 255)
 
 	//取text及长度

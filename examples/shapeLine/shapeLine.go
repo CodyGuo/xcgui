22c6671107fb6f12d136053c146f8a76081637f3
CodyGuo <guoxiao219@126.com>
2015-12-08 13:21:51 +0800

    新增shapeLine例子

---
+++ examples/shapeLine/shapeLine.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	xcgui "github.com/codyguo/xcgui/xc"
+)
+
+func main() {
+	hWindow := xcgui.XWndCreate(0, 0, 400, 300, "炫彩界面库窗口", 0, xcgui.XC_WINDOW_STYLE_DEFAULT)
+	xcgui.CloseBtn(hWindow)
+
+	// hShapeLine
+	xcgui.XShapeLineCreate(20, 50, 100, 100, xcgui.HXCGUI(hWindow))
+	xcgui.XWndShowWindow(hWindow, xcgui.SW_SHOW)
+	xcgui.XRunXCGUI()
+	xcgui.XExitXCGUI()
+}

3430d6ce32065e353b74c4d13b8d63de3a1833da
CodyGuo <guoxiao219@126.com>
2015-12-13 16:42:09 +0800

    新增下载dll时的友好提示

--- xc/download.go
+++ xc/download.go
@@ -9,7 +9,13 @@ import (
 	"os"
 )
 
+import (
+	"github.com/lxn/walk"
+)
+
 func downLoadXCGUIDll() {
+	mw, _ := walk.NewMainWindow()
+
 	resp, err := http.Get("http://git.oschina.net/CodyGuo/xcgui/raw/master/examples/lib/XCGUI.dll")
 	if err != nil {
 		log.Fatal(err)
@@ -17,6 +23,13 @@ func downLoadXCGUIDll() {
 
 	if resp.StatusCode == http.StatusOK {
 		log.Println("[INFO] 正在下载 XCGUI.dll .")
+		ret := walk.MsgBox(mw, "下载提示", "没有找到XCGUI.dll，是否要联网下载？", walk.MsgBoxIconQuestion+walk.MsgBoxOKCancel)
+		if ret != 1 {
+			walk.MsgBox(mw, "警告信息", "请在重新运行程序前将XCGUI.dll放到运行目录.", walk.MsgBoxIconWarning)
+			mw.Close()
+			os.Exit(1)
+		}
+
 		downFile, err := os.Create("XCGUI.dll")
 		if err != nil {
 			log.Fatal(err)
@@ -27,8 +40,15 @@ func downLoadXCGUIDll() {
 		body, err := ioutil.ReadAll(resp.Body)
 		io.Copy(downFile, bytes.NewReader(body))
 		log.Println("[INFO] XCGUI.dll 下载成功.正在运行主程序.")
+
+		walk.MsgBox(mw, "提示信息", "下载成功, 请点击确定继续运行程序.", walk.MsgBoxIconInformation)
+		mw.Close()
 	} else {
 		log.Printf("[ERROR] 下载失败,%s.\n", resp.Status)
+
+		walk.MsgBox(mw, "错误信息", "下载失败.", walk.MsgBoxIconError)
+		mw.Close()
 	}
 
+	mw.Run()
 }
